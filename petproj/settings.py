6fc6dfa1bf80c018ac2dc91bffb61594025b2c9f
Oleksii <aleksejlitovcenko69@gmail.com>
2025-10-19 19:46:28 +0300

    background

--- petproj/settings.py
+++ petproj/settings.py
@@ -129,6 +129,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Default primary key field type
